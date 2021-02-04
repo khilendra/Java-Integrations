72e9442faaa89ad0a42e35e4e3aed6c35658568c
khilendra9 <khilendra.sahu443@gmail.com>
2021-02-04 09:42:19 +0530

    new file

---
+++ src/com/iriand/email/EmailInterface.java
@@ -0,0 +1,9 @@
+package com.iriand.email;
+
+/**
+ * Created By Khilendra Sahu
+ * 1/1/21 10:56 PM
+ */
+public interface EmailInterface {
+    void send(String from, String[] recipientsTo, String[] recipientsCC, String[] recipientsBCC, String subject, String body, String[] files);
+}
